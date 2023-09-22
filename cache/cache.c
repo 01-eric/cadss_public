6f6e719c84b9f261a14ac7208a9b9bd0f89f89a0
Eric Fan <ericfan@linux-01.andrew.cmu.edu>
2023-09-22 15:55:21 -0400

    working baseline cache, TODO: RRIP and victim cache

--- cache/cache.c
+++ cache/cache.c
@@ -1,6 +1,7 @@
-#include <cache.h>
-#include <trace.h>
-
+#include "csim.h"
+#include "trace.h"
+#include <stdio.h>
+#include <stdbool.h>
 #include <getopt.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -11,59 +12,123 @@ typedef struct _pendingRequest {
     void (*memCallback)(int, int64_t);
 } pendingRequest;
 
-cache* self = NULL;
+csim* self = NULL; // csim is the cache object, see csim.h for definition
 int processorCount = 1;
 int CADSS_VERBOSE = 0;
 pendingRequest pending = {0};
 int countDown = 0;
+int E = -1; // associativity
+int s = -1; // # of set bits, and S = 2^s gives number of sets
+int b = -1; // # of block bits, and B = 2^b gives number of block bytes
+int i = -1; // # of lines in victim cache
+int k = -1; // # of bits in the RRPV, max value of RRPV is 2^k - 1
 
 void memoryRequest(trace_op* op, int processorNum, int64_t tag,
                    void (*callback)(int, int64_t));
 
-cache* init(cache_sim_args* csa)
+int pow2(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return 2 * pow2(n - 1);
+}
+
+csim* init(cache_sim_args* csa)
 {
+    extern char *optarg;
     int op;
 
-    // TODO - get argument list from assignment
+    // process arguments to fill in the parameters of the cache
     while ((op = getopt(csa->arg_count, csa->arg_list, "E:s:b:i:R:")) != -1)
     {
         switch (op)
         {
             // Lines per set
             case 'E':
+                E = atoi(optarg);
                 break;
 
             // Sets per cache
             case 's':
+                s = atoi(optarg);
                 break;
 
             // block size in bits
             case 'b':
+                b = atoi(optarg);
                 break;
 
             // entries in victim cache
             case 'i':
+                i = atoi(optarg);
                 break;
 
             // bits in a RRIP-based replacement policy
             case 'R':
-
+                k = atoi(optarg);
                 break;
         }
-    }
+    } printf("Input parameters: E = %d, s = %d, b = %d, i = %d, k = %d\n", E, s, b, i, k);
 
-    self = malloc(sizeof(cache));
+    // initialize the cache here
+    self = malloc(sizeof(csim));
     self->memoryRequest = memoryRequest;
     self->si.tick = tick;
     self->si.finish = finish;
     self->si.destroy = destroy;
-
+    int S = pow2(s);
+    int B = pow2(b);
+    line **cache = calloc(sizeof(line*), S);
+    cache = calloc(sizeof(line*), S);
+    for (int i = 0; i < S; i++) {
+        cache[i] = calloc(sizeof(line), E);
+        for (int j = 0; j < E; j++) {
+            cache[i][j].vbit = 0;
+            cache[i][j].dbit = 0;     // disregarded if vbit == 0
+            cache[i][j].lastused = 0; // disregarded if vbit == 0
+            cache[i][j].tag = 0;      // disregarded if vbit == 0
+        }
+    } self->cache = cache;
+    printf("Initialized cache of %d x %d x %d\n", S, E, B);
     return self;
+}
+
+void handleHit(int set, int index, bool store) {
+    line *l = &self->cache[set][index]; 
+    l->lastused = 0;
+    if (store && l->dbit == 0) l->dbit = 1;
+    printf("Got cache hit in set %d at index %d\n", set, index);
+    countDown = 1;
+    // TODO: do additional check for hit in victim cache
+}
+
+void handleColdMiss(unsigned long tag, int set, int index, bool store) {
+    line *l = &self->cache[set][index];
+    l->lastused = 0;
+    l->tag = tag;
+    l->vbit = 1;
+    if (store) l->dbit = 1;
+    printf("Got cold cache miss, loaded into set %d at index %d\n", set, index);
+    countDown = 100;
+}
+
+void handleConflictMiss(unsigned long tag, int set, int index, bool store) {
+    line *l = &self->cache[set][index];
+    l->lastused = 0;
+    l->tag = tag;
+    if (l->dbit == 1) {
+        l->dbit = 0;
+        countDown = 150;
+    } else countDown = 100;
+    if (store) l->dbit = 1;
+    printf("Got conflict cache miss, evicted entry in set %d at index %d\n", set, index);
+    // TODO: move evicted entry to victim cache
 }
 
 void memoryRequest(trace_op* op, int processorNum, int64_t tag,
                    void (*callback)(int, int64_t))
 {
+    printf("Received %s instruction for address 0x%lx\n", op->op == 1 ? "load" : "store", op->memAddress);
     assert(op != NULL);
     assert(callback != NULL);
 
@@ -77,9 +142,32 @@ void memoryRequest(trace_op* op, int processorNum, int64_t tag,
     pending = (pendingRequest){
         .tag = tag, .procNum = processorNum, .memCallback = callback};
 
-    // In a real cache simulator, the delay is based
-    // on whether the request is a hit or miss.
-    countDown = 1;
+    // handle the cache operation here to determine countdown based on hit or miss
+    unsigned long addr = op->memAddress;
+    bool store = op->op == MEM_STORE;
+    addr >>= b;
+    unsigned long cacheTag = addr >> s;
+    unsigned long set = s == 0 ? 0 : addr << (64 - s) >> (64 - s);
+    printf("Operation type: %s, tag: 0x%lx, set: %lu, ", store ? "store" : "load", cacheTag, set);
+    int emptyIndex = -1; // denotes cold miss if not -1
+    int matchIndex = -1; // denotes cache hit if not -1
+    int LRU = 0; // if neither cache hit or cold miss, then must be conflict miss
+    int LRUIndex = -1; // use LRUIndex in case of conflict miss
+    for (int j = 0; j < E; j++) {
+        line *l = &self->cache[set][j];
+        if (l->vbit == 0 && emptyIndex == -1) emptyIndex = j;
+        else if (l->vbit == 1) {
+            if (l->tag == cacheTag && matchIndex == -1) matchIndex = j;
+            if (++l->lastused > LRU) {
+                LRU = l->lastused;
+                LRUIndex = j;
+            }
+        }
+    } printf("match index: %d, empty index: %d, LRU value: %d, LRU index: %d\n", matchIndex, emptyIndex, LRU, LRUIndex);
+    if (matchIndex != -1) handleHit(set, matchIndex, store);
+    else if (emptyIndex != -1) handleColdMiss(cacheTag, set, emptyIndex, store);
+    else handleConflictMiss(cacheTag, set, LRUIndex, store);
+    printf("Setting countdown to %d\n\n", countDown);
 }
 
 int tick()
---
+++ cache/csim.h
@@ -0,0 +1,27 @@
+#ifndef CSIM_H
+#define CSIM_H
+
+#include "trace.h"
+#include "coherence.h"
+
+typedef struct _cache_sim_args {
+    int arg_count;
+    char** arg_list;
+    coher* coherComp;
+} cache_sim_args;
+
+// representation of a single cache line
+typedef struct {
+    short vbit;
+    short dbit;
+    int lastused;
+    unsigned long tag;
+} line;
+
+typedef struct _csim {
+    sim_interface si;
+    void (*memoryRequest)(trace_op*, int, int64_t, void(*callback)(int, int64_t));
+    line **cache;
+} csim;
+
+#endif
